8cac0a8aad1ba3e5a66bbde3ba3005b9051d5c08
John Kwiatkoski <jkwiatkoski@protonmail.com>
2025-05-25 15:14:57 -0400

    adding output option and add json support

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"dup/pkg/config"
 	"dup/pkg/duplicate"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log/slog"
@@ -12,8 +13,8 @@ import (
 )
 
 func main() {
-	var recurse, verbose, help bool
-	var dirs string
+	var recurse, verbose, hashes, help bool
+	var dirs, output string
 	var loglevel slog.LevelVar
 	var logger *slog.Logger
 	logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
@@ -26,14 +27,21 @@ func main() {
 	flag.BoolVar(&verbose, "v", false, "enable verbose logging.")
 	flag.BoolVar(&help, "h", false, "display help message.")
 	flag.StringVar(&dirs, "d", ".", "directories to search for duplicate files.")
+	flag.StringVar(&output, "o", "text", "display mode for results. options: \"text\", \"json\"")
+	flag.BoolVar(&hashes, "x", false, "whether to change the resulting map's keys to the file hashes. Otherwise the first file processed with that hash is used as the key.")
+
 	flag.Parse()
 
+	if help {
+		flag.PrintDefaults()
+		return
+	}
 	err := conf.SetDirectories(dirs)
 	if err != nil {
 		slog.Error("setting directories", "err", err)
 	}
 
-	if len(conf.Directories) == 0 || help {
+	if len(conf.Directories) == 0 {
 		slog.Error("no directories specified")
 		flag.PrintDefaults()
 		return
@@ -69,10 +77,47 @@ func main() {
 		}()
 	}
 	wg.Wait()
-	fmt.Print(ProcessFileMap())
+	switch output {
+	case "text":
+		fmt.Print(TextFileMap())
+
+	case "json":
+		fmt.Print(JSONFileMap(hashes))
+
+	default:
+		fmt.Print(TextFileMap())
+	}
 }
 
-func ProcessFileMap() string {
+func JSONFileMap(hashes bool) string {
+	fileMap := duplicate.GetHashMap().Map
+	var marshalTarget map[string][]string
+	if hashes {
+		// use hashes as keys and all files as list
+		marshalTarget = map[string][]string{}
+		for hash, list := range fileMap {
+			if len(list) > 1 {
+				marshalTarget[hash] = list
+			}
+		}
+	} else {
+		// use the first file as a key and rest as list
+		marshalTarget = map[string][]string{}
+		for _, list := range fileMap {
+			if len(list) > 1 {
+				marshalTarget[list[0]] = list[1:]
+			}
+		}
+	}
+	js, err := json.Marshal(marshalTarget)
+	if err != nil {
+		slog.Error("marshalling filemap", "map", fileMap, "err", err)
+	}
+	return string(js)
+
+}
+
+func TextFileMap() string {
 	fileMap := duplicate.GetHashMap()
 	sb := strings.Builder{}
 
